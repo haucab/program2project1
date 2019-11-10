cb700c80903aa7ed1a6ee5f6800119b221744adb
HAUCAB <hacuenca.19@est.ucab.edu.ve>
2019-11-10 12:13:06 -0400

    Validation of arguments in order to conform to spec.

--- sucursal_validate.h
+++ sucursal_validate.h
@@ -24,6 +24,7 @@ ValidationError_Sucursal validateSucursal(struct Sucursal** cabeza,
     if (validationTotal != SUCURSAL_OK) return validationTotal;
 
     return validateSucursalEmail(email);
+    //
 }
 
 #endif //PROJECT1_SUCURSAL_VALIDATE_H
